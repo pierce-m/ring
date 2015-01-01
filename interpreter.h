a81b84e43ce76463bff05454dfdca15851f335e8
Pierce McEntagart <pierce.mt92@gmail.com>
2014-12-31 22:10:51 -0800

    Removed files from old source tree structure (which was no structure).

--- interpreter.h
+++
@@ -1,8 +0,0 @@
-#include "node.h"
-
-/* Evaluates expression nodes based on type */
-int eval_expr (node_expr_t *exp);
-
-/* Evaluator for arithmetic expressions */
-int eval_arith (node_arith_t *node);
-
