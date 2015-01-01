b1075ed5d99a01f2d9717cd2f92198bd9886bfce
Pierce McEntagart <pierce.mt92@gmail.com>
2014-12-31 16:54:03 -0800

    Added some environment functionality for variable assignment.

--- src/environment.c
+++ src/environment.c
@@ -1,23 +1,26 @@
 #include "environment.h"
+#include <stdlib.h>
 
 void
-env_add_binding (char *name, ring_type value, env *e) {
+env_add_binding (char *name, ring_type *value, env *e) {
     hash_insert (name, value, e->bindings);
 }
 
 //Still needs to handle not-found errors
-ring_type
+ring_type*
 env_lookup (char *name, env *e) {
     ring_type *val;
-    if ((val = hash_get (name, e)) != NULL) {
+    if ((val = hash_get (name, e->bindings)) != NULL) {
         return val;
     } else if (e->parent != NULL) {
         return env_lookup (name, e->parent);
+    } else {
+        return NULL;
     }
 }
 
-env
-*env_extend (env *e) {
+env*
+env_extend (env *e) {
     env *newEnv = malloc (sizeof (env));
     e->parent = e;
     e->bindings = malloc (sizeof (hashtable));
--- src/environment.h
+++ src/environment.h
@@ -1,5 +1,5 @@
 #include "datatypes.h"
-#include "lib/hashtable.h"
+#include "../lib/hashtable.h"
 
 /* An environment defines a lexical scope. It is composed of key-value bindings
  * and a pointer to its parent. The parent of the root environment is NULL */
@@ -10,7 +10,7 @@ typedef struct env {
 
 /* Creates a binding for the char *name in e. If one already exists, the previous
  * binding is overwritten */
-void env_add_binding (char *name, ring_typ value, env *e);
+void env_add_binding (char *name, ring_type *value, env *e);
 
 /* Looks in the current environment for the value bound to name. */
 ring_type *env_lookup (char *name, env *e);
