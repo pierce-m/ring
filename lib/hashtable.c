e6300dd9152a3f9114fa99328d3519b4d22d2866
Pierce McEntagart <pierce.mt92@gmail.com>
2014-12-31 16:54:31 -0800

    Added a hashtable implementation to support environment data.

--- lib/hashtable.c
+++ lib/hashtable.c
@@ -1,20 +1,87 @@
 #include "hashtable.h"
+#include <string.h>
+#include <stdlib.h>
 
 unsigned long
 hash (unsigned char *str) {
     unsigned long hash = 5381;
     int c;
-    while (c = *str++)
+    while ((c = *str++))
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     return hash;
 }
 
-void
-hash_insert (unsigned char *str, ring_type value, hashtable *h) {
-    list_insert (hash (str) % h->size, value); 
+hashtable*
+hashtable_init (int size) {
+    hashtable *h = malloc (sizeof (hashtable));
+    h->size = size;
+    h->buckets = malloc (sizeof (hash_pair *) * size);
+    int i;
+    for (i = 0; i < 10; ++i) {
+        h->buckets[i] = NULL;
+    }
+    return h;
 }
 
-ring_type*
-hash_get (unsigned char *str, hashtable *h) {
-    return list_get (hash (str) % h->size, str);
+void hash_chain_destroy (hash_pair *hp) {
+    if (hp->next == NULL) {
+        free (hp->key);
+        free (hp->val);
+        free (hp);
+    } else {
+        hash_chain_destroy (hp->next);
+        hash_chain_destroy (hp);
+    }
+}
+
+void
+hashtable_destroy (hashtable *h) {
+    int i;
+    for (i = 0; i < h->size; ++i)
+        hash_chain_destroy (h->buckets[i]);
+    free (h->buckets);
+    free (h);
+}
+
+hash_pair*
+hash_pair_init (char *str, void *val) {
+    hash_pair *hp = malloc (sizeof (hash_pair));
+    hp->key = str;
+    hp->val = val;
+    hp->next = NULL;
+    return hp;
+}
+
+hash_pair*
+hash_chain_end (hash_pair *hp) {
+    for (; hp->next != NULL; hp = hp->next);
+    return hp;
+}
+
+hash_pair*
+hash_pair_get (char *str, hashtable *h) {
+    hash_pair *hp = h->buckets[hash ((unsigned char *) str) % h->size];
+    if (hp == NULL)
+        return NULL;
+    for (; hp->next != NULL; hp = hp->next)
+        if (!strcmp(hp->key, str))
+            return hp;
+    return NULL;
+}
+
+void
+hash_insert (char *str, void *value, hashtable *h) {
+    hash_pair *existing_val;
+    if ((existing_val = hash_pair_get (str, h)) != NULL) {
+        existing_val->val = value;
+    } else {
+        hash_chain_end (h->buckets[hash ((unsigned char*) str) % h->size])->next =
+            hash_pair_init (str, value);
+    }
+}
+
+void*
+hash_get (char *str, hashtable *h) {
+    hash_pair *hp = hash_pair_get (str, h);
+    return hp == NULL ? hp : hp->val;
 }
--- lib/hashtable.h
+++ lib/hashtable.h
@@ -1,30 +1,35 @@
-#include "list.h"
-#include "src/datatypes.h"
-
 #define MAX_CHAIN_SIZE 7
 
-/* Implementation of a hashtable. Initial size is 10 in anticipation of
- * low number of environment bindings. Collisions are avoided via chaining. */
-typedef struct hashtable {
-    int size = 10;
-    int chain_size = 0;
-    struct list *buckets[10];
-} hashtable;
-
+/* Item type for entry in the lists which back hashtable buckets */
 typedef struct hash_pair {
     char *key;
-    ring_type value;
-}
+    void *val;
+    struct hash_pair *next;
+} hash_pair;
+
+/* Implementation of a hashtable. Has a size corresponding to number of
+ * buckets. */
+typedef struct hashtable {
+    int size;
+    struct hash_pair **buckets;
+} hashtable;
 
 /* Returns a hash of a string based on djb2 -- found here:
  * www.cse.yorku.ca/~oz/hash.html */
 unsigned long hash (unsigned char *str);
 
-/* Inserts a key-value pair into hashtable h */
-void hash_insert (unsigned char *str, ring_type value, hashtable *h);
+/* Initializes a new hashtable with size buckets */
+hashtable *hashtable_init (int size);
 
-/* Retrieves a the value corresponding to key from hashtable h */
-ring_type *hash_get (unsigned char *str, hashtable *h);
+/* Frees all memory assiciated with the hash table */
+void hashtable_destroy (hashtable *h);
+
+/* Inserts a key-value pair into hashtable h */
+void hash_insert (char *str, void *value, hashtable *h);
+
+/* Retrieves a the value corresponding to key from hashtable h. Returns NULL
+ * if a key value pair is not found. */
+void *hash_get (char *str, hashtable *h);
 
 /* Rehashes the hashtable if the chaining lists grow too large to be
  * efficient */
