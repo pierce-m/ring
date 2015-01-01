2900ebec41cee6e4d79098567a770f00bcee97ad
Pierce McEntagart <pierce.mt92@gmail.com>
2015-01-01 10:39:43 -0800

    Added util files for the parser.

---
+++ src/parser_utils.c
@@ -0,0 +1,34 @@
+#include "parser_utils.h"
+
+char*
+copy_lexeme (char *lexeme) {
+    char *copy = malloc (sizeof (lexeme));
+    strcpy (copy, lexeme);
+    return copy;
+}
+
+void
+interpret_file (FILE *f) {
+    in = 0;
+    yyin = f;
+    yyparse ();
+    interpret ();
+}
+
+void
+run_interpretive () {
+    int res = 1;
+    in = 1;
+    yyin = stdin;
+    printf ("%s ", (char *) "ring>");
+    while (res) { res = yyparse (); }
+}
+
+void
+execute_or_append (node_statement_t *s) {
+    add_statement (s);
+    if (in) {
+        interpret ();
+        printf ("%s ", (char *) "ring>");
+    }
+}
---
+++ src/parser_utils.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "node.h"
+
+/* Interpretive mode */
+int in;
+
+extern FILE *yyin;
+
+/* Creates a copy of a lexeme. */
+char *copy_lexeme (char *lexeme);
+
+/* Interprets the file pointed to by f */
+void interpret_file (FILE *f);
+
+/* Runs the REPL */
+void run_interpretive ();
+
+/* Depending on if we're in the REPL, executes a statement or adds
+ * in to the AST */ 
+void execute_or_append (node_statement_t *s);
+
